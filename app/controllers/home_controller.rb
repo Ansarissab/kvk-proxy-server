157a92e142aa28b0c7622bb7946708fb014ef2dd
Ansarissab <zahidensari116@gmail.com>
2022-02-11 19:53:05 -0500

    Added a sync data request from kvk api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
+require 'net/http'
 class HomeController < ApplicationController
-  def index
+  def index; end
+
+  def sync
+    number = 90_005_368
+    url = "https://developers.kvk.nl/api/v2/search/companies?kvkNumber=#{number}"
+    uri = URI(url)
+    @response = Net::HTTP.get(uri)
+    @response = @response.to_json
+    respond_to do |format|
+      format.js
+    end
+    # render json: response.to_json
   end
 end
